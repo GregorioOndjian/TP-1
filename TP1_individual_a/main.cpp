2a04bed0e3ef17303ab8b357a88e321efa3f012e
Gregorio Manuel Ondjian <82954160+GregorioOndjian@users.noreply.github.com>
2021-04-28 10:36:46 -0300

    Add files via upload

---
+++ TP1_individual_a/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int vel;
+    cout << "Ingrese una velocidad(numero) en K/H" << endl;
+    cin >> vel;
+    cout << "La velocidad es " << vel/3.6 << " M/S" <<endl;
+}
---
+++ TP1_individual_b/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a1,b1,a2,b2,Xf,Yf,a;
+    cout << "Ingrese 2 pendientes " << endl;
+    cin >> a1 >> a2;
+    a = a1;
+     if (a1 == a2 || a1 == 0 || a2 == 0){
+        cout << "Las rectas no se intersecan ya que son paralelas, o alguna de las 2 no existe" << endl;
+    }
+    else{
+    cout << "Ingrese 2 ordenadas al origen " << endl;
+    cin >> b1 >> b2;
+    if(b1>=0){
+        b2 = b2 -b1;
+    }
+    else{
+        b1 =b1*-1;
+        b2 = b2 + b1;
+    }
+    if (a1>0){
+        a1 = a1 -a2;
+    }
+    else{
+        a2 = a2 *-1;
+        a1 = a1+a2;
+    }
+    if (a1<0){
+        a1 = a1 *-1;
+        b2 = b2*-1;
+    }
+    Xf = b2/a1;
+    Yf = (a*Xf)+b1;
+    cout << "El punto de interseccion es " <<"("<<Xf<<","<<Yf<<")" <<endl;
+    }
+
+}
